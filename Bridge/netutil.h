ec835e07970125a3704e53ae857dcdf60fcfc99f
ueda <ueda@localhost.localdomain>
2020-10-03 13:55:50 +0900

    フォーマットを修正

--- Bridge/netutil.h
+++
@@ -1,3 +0,0 @@
-char *my_ether_ntoa_r(u_char *hwaddr,char *buf,socklen_t size);
-int PrintEtherHeader(struct ether_header *eh,FILE *fp);
-int InitRawSocket(char *device,int promiscFlag,int ipOnly);
--- Router/sendBuf.h
+++
@@ -1,4 +0,0 @@
-int AppendSendData(IP2MAC *ip2mac,int deviceNo,in_addr_t addr,unsigned char *data,int size);
-int GetSendData(IP2MAC *ip2mac,int *size,unsigned char **data);
-int FreeSendData(IP2MAC *ip2mac);
-int BufferSend();
